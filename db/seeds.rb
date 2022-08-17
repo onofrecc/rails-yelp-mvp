a1baa7c0ba9812cd03b6b0e58d2fc9b699494f6c
Onofre Catala Castellar <onofrecc@gmail.com>
2022-08-17 14:22:56 +0200

    Seeds ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Deleting all records in the database....'
+Restaurant.destroy_all
+
+puts 'Creating restaurants and reviews...'
+5.times do
+  category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: category.sample
+  )
+  restaurant.save!
+
+  rand(0..10).times do
+    review = Review.new(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review,
+      restaurant_id: restaurant.id
+    )
+    review.save!
+  end
+end
+
+puts 'Done for now! Go ahead with your coding!'
